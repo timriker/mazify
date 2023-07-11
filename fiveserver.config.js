a8f29816b731ad1220878d1d25b7621af371abe1
Tim Riker <timriker@gmail.com>
2023-07-11 22:33:15 +0000

    config

---
+++ fiveserver.config.js
@@ -0,0 +1,11 @@
+// fiveserver.config.js
+module.exports = {
+  port: 443,
+  root: '.',
+  //open: ['about.html', 'contact.html'],
+  host: '0.0.0.0',
+  //browser: 'firefox',
+  remoteLogs: true,
+  https: true,
+  useLocalIp: true
+}
